8b55938f1d2fa3fb385f465fa3d2bbcc946766df
RONAK <ronak192006@gmail.com>
2026-08-16 18:06:33 +0530

    added location icon

--- script.js
+++ script.js
@@ -122,9 +122,163 @@ search_input.addEventListener("keydown", (e) => {
     }
 });
 
+// CURRENT LOCATION
 
+search_location.addEventListener("click", () => {
+    if (!navigator.geolocation) {
+        alert("Geolocation is not supported by your browser.");
+        return;
+    }
 
+    // Get current GPS position
+    navigator.geolocation.getCurrentPosition(
+        async (position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
 
+            console.log("Latitude:", lat);
+            console.log("Longitude:", lon);
 
+            try {
+                //  GET WEATHER USING GPS COORDINATES
+                const weatherURL =
+                    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
 
+                const weatherResponse = await fetch(weatherURL);
 
+                if (!weatherResponse.ok) {
+                    throw new Error("Unable to fetch weather data");
+                }
+                const weatherData = await weatherResponse.json();
+                console.log("Weather Data:", weatherData);
+
+                // REVERSE GEOCODING
+                const locationURL =
+                    `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lon}&zoom=18&addressdetails=1`;
+
+                const locationResponse = await fetch(locationURL);
+                if (!locationResponse.ok) {
+                    throw new Error("Unable to fetch location name");
+                }
+
+                const locationData = await locationResponse.json();
+
+                console.log("Location Data:", locationData);
+                console.log("Address:", locationData.address);
+
+                // FIND MOST PRECISE LOCALITY
+
+                const address = locationData.address;
+                const locationName =
+                    address.neighbourhood ||
+                    address.suburb ||
+                    address.quarter ||
+                    address.city_district ||
+                    address.town ||
+                    address.city ||
+                    "Current Location";
+
+                // Put location name in search box
+                search_input.value = locationName;
+
+                // WEATHER INFORMATION
+                temperature.innerHTML =
+                    `${Math.round(weatherData.main.temp)}<span>°C</span>`;
+
+                description.textContent =
+                    weatherData.weather[0].description;
+
+                humidity_value.textContent =
+                    `${weatherData.main.humidity}%`;
+
+                wind_value.textContent =
+                    `${Math.round(weatherData.wind.speed)} km/h`;
+
+                // 5. WEATHER ICON + BACKGROUND
+                const weathercondition =
+                    weatherData.weather[0].main;
+
+                const body = document.body;
+
+                if (weathercondition === "Clear") {
+                    weather_icon.src = "clear.png";
+                    body.style.setProperty(
+                        "--weather-bg",
+                        "url('clear-background.png')"
+                    );
+                }
+
+                else if (weathercondition === "Clouds") {
+                    weather_icon.src = "cloud.png";
+                    body.style.setProperty(
+                        "--weather-bg",
+                        "url('cloud-background.png')"
+                    );
+
+                }
+                else if (weathercondition === "Snow") {
+                    weather_icon.src = "snow.png";
+                    body.style.setProperty(
+                        "--weather-bg",
+                        "url('snow-background.png')"
+                    );
+                }
+
+                else if (weathercondition === "Rain") {
+                    weather_icon.src = "rain.png";
+                    body.style.setProperty(
+                        "--weather-bg",
+                        "url('rain-background.png')"
+                    );
+                }
+
+                else if (weathercondition === "Mist") {
+                    weather_icon.src = "mist.png";
+                    body.style.setProperty(
+                        "--weather-bg",
+                        "url('mist-background.png')"
+                    );
+                }
+                else {
+                    weather_icon.src = "404.png";
+                    body.style.setProperty(
+                        "--weather-bg",
+                        "url('404.png')"
+                    );
+                }
+                // LOG FINAL LOCATION
+                console.log("Detected Location:", locationName);
+            }
+            catch (error) {
+                console.log("ERROR:", error);
+                alert("Unable to fetch your location/weather.");
+            }
+        },
+
+        // LOCATION ERROR
+
+        (error) => {
+            console.log(error);
+            if (error.code === error.PERMISSION_DENIED) {
+                alert("Please allow location permission.");
+            }
+            else if (error.code === error.POSITION_UNAVAILABLE) {
+                alert("Location information is unavailable.");
+            }
+            else if (error.code === error.TIMEOUT) {
+                alert("Location request timed out.");
+            }
+            else {
+                alert("Unable to get your location.");
+            }
+        },
+
+        // GPS OPTIONS 
+        {
+            enableHighAccuracy: true,
+            timeout: 10000,
+            maximumAge: 0
+        }
+
+    );
+});
